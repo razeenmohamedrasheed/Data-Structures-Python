aea65948245dcef9ebf82f4d6e68c806852c9e88
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2023-12-23 18:37:24 +0530

    patterns

---
+++ Patterns/pattern1.py
@@ -0,0 +1,14 @@
+# *
+# **
+# ***
+# ****
+# *****
+
+# code for the above pattern
+
+rows= int(input("enter the number of rows"))
+
+for i in range(rows): #to print the number of rows
+    for j in range(i+1):
+        print("*",end="") #to print the pattern
+    print()               #break after each each print to come to the next line   
