e344fd625c5bc523f53720bd89abe9fe26a8deab
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2023-12-27 13:10:20 +0530

    updates

--- Patterns/numberpattern5.py
+++ Patterns/numberpattern5.py
@@ -6,8 +6,10 @@
 # 11 12 13 14 15
 
 rows = int(input("enter the number of rows:"))
+num = 1
 
 for i in range(1,rows + 1):
     for j in range(1,i+1):
-        print(j+1,end="")
+        print(num,end="")
+        num = num +1
     print()
---
+++ Patterns/starpattern.py
@@ -0,0 +1,16 @@
+#      *
+#     **
+#    ***
+#   ****
+# *****
+
+# code for the reverse pattern
+
+rows= int(input("enter the number of rows"))
+
+for i in range(1,rows+1):               
+    for j in range(rows+1,i,-1):
+        print(" ",end="")   
+    for k in range(1,i+1):
+        print("*",end="")        
+    print()                        
