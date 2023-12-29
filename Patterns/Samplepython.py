2670f13cfdc8af4d2d4b8a50e2e950326f4e5067
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2023-12-29 21:08:28 +0530

    Update Samplepython.py

--- Patterns/Samplepython.py
+++ Patterns/Samplepython.py
@@ -26,9 +26,4 @@ for i in range(1,rows+1):
     print()                        
     rows= int(input("enter the number of rows"))
 
-for i in range(1,rows+1):               
-    for j in range(rows+1,i,-1):
-        print(" ",end="")   
-    for k in range(1,i+1):
-        print("*",end="")        
-    print()                        
+         
