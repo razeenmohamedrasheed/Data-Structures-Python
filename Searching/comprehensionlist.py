54775c12c8176c4853b942b51c40fd43e1bf3159
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-01-07 21:16:12 +0530

    Update comprehensionlist.py

--- Searching/comprehensionlist.py
+++ Searching/comprehensionlist.py
@@ -1,11 +1,4 @@
 new_list = [ i for i in range(0,100)]
 print(new_list)
 
-new_list = [ i for i in range(0,100)]
-print(new_list)
-new_list = [ i for i in range(0,100)]
-print(new_list)
-new_list = [ i for i in range(0,100)]
-print(new_list)
-new_list = [ i for i in range(0,100)]
-print(new_list)
+
