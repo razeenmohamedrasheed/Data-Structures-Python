490181d620bc531483c9b251c055569e9e4062cb
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-01-27 12:09:36 +0530

    Create 1662.py

---
+++ Leetcode/1662.py
@@ -0,0 +1,15 @@
+# Example 1:
+
+# Input: word1 = ["ab", "c"], word2 = ["a", "bc"]
+# Output: true
+# Explanation:
+# word1 represents string "ab" + "c" -> "abc"
+# word2 represents string "a" + "bc" -> "abc"
+# The strings are the same, so return true.
+
+
+class Solution:
+    def arrayStringsAreEqual(self, word1: List[str], word2: List[str]) -> bool:
+        return ''.join(word1) == ''.join(word2)
+
+        
