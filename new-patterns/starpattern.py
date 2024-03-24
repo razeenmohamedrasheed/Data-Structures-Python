1cec510b623b26d10aa90c50f0a67dd422b8806c
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-03-24 09:22:44 +0530

    Patterns

---
+++ new-patterns/starpattern.py
@@ -0,0 +1,20 @@
+# *
+# **
+# ***
+# ****
+# *****
+
+# Basic Method
+for i in range(6):
+    for j in range(i+1):
+        print("*",end="")
+    print()
+
+# Taking Input Value and Design
+    
+row = int(input("enter the number of rows"))
+
+for i in range(row):
+    for j in range(i+1):
+        print("*",end="")
+    print()
