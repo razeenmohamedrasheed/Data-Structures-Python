773dbceb863cf3a440fd643dd0978f22a0999cb5
Razeen Mohamed Rasheed <razeenrasheed83@gmail.com>
2024-03-24 13:58:26 +0530

    added

---
+++ new-patterns/numberPatterns.py
@@ -0,0 +1,14 @@
+# 1
+# 22
+# 333
+# 4444
+# 55555
+
+# Taking Input Value and Design
+    
+row = int(input("enter the number of rows"))
+
+for i in range(1,row):
+    for j in range(1,i+1):
+        print(i,end="")
+    print()
